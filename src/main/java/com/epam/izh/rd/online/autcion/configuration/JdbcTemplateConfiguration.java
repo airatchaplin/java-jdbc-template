e724a2ab8814ae204fa7c87d884ad97cf006b1c8
Airat_Salikhov <airatchaplin@gmail.com>
2020-11-03 19:54:04 +0400

    Готово

--- src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
+++ src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
@@ -3,19 +3,27 @@ package com.epam.izh.rd.online.autcion.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.ResourceBundle;
 
 @Configuration
 public class JdbcTemplateConfiguration {
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return null;
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
     @Bean
     public DataSource dataSource() {
-        return null;
+        ResourceBundle rb = ResourceBundle.getBundle("application");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(rb.getString("spring.datasource.driverClassName"));
+        dataSource.setUrl(rb.getString("spring.datasource.url"));
+        dataSource.setUsername(rb.getString("spring.datasource.username"));
+        dataSource.setPassword(rb.getString("spring.datasource.password"));
+        return dataSource;
     }
 }
--- src/main/java/com/epam/izh/rd/online/autcion/entity/Bid.java
+++ src/main/java/com/epam/izh/rd/online/autcion/entity/Bid.java
@@ -10,12 +10,38 @@ import java.time.LocalDate;
  * Ставка
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Bid {
     private Long bidId;
     private LocalDate bidDate;
     private Double bidValue;
     private Long itemId;
     private Long userId;
+
+    public Bid(Long bidId, LocalDate bidDate, Double bidValue, Long itemId, Long userId) {
+        this.bidId = bidId;
+        this.bidDate = bidDate;
+        this.bidValue = bidValue;
+        this.itemId = itemId;
+        this.userId = userId;
+    }
+
+    public Long getBidId() {
+        return bidId;
+    }
+
+    public LocalDate getBidDate() {
+        return bidDate;
+    }
+
+    public Double getBidValue() {
+        return bidValue;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
 }
